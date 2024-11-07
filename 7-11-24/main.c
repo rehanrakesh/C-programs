9d2e70a9889725156fec3eb6f7d042798788106d
rehanrakesh <152681653+rehanrakesh@users.noreply.github.com>
2024-11-07 23:03:29 +0530

    Add files via upload

---
+++ 7-11-24/main.c
@@ -0,0 +1,23 @@
+/******************************************************************************
+
+Welcome to GDB Online.
+  GDB online is an online compiler and debugger tool for C, C++, Python, PHP, Ruby, 
+  C#, OCaml, VB, Perl, Swift, Prolog, Javascript, Pascal, COBOL, HTML, CSS, JS
+  Code, Compile, Run and Debug online from anywhere in world.
+
+*******************************************************************************/
+#include <stdio.h>
+
+int main()
+{
+    int a, b;
+    printf("Enter first No :");
+    scanf("%d",&a);
+    printf("Enter second No :");
+    scanf("%d",&b);
+    printf("Sum is %d",a+b);
+    printf(" ,Difference is %d",a-b);
+    printf(" ,Product is %d",a*b);
+    printf(" ,Quotient is %d",a/b);
+    return 0;
+}
